c5d1037af69dae7d1e5b9d5e165a30237ed39e57
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-10-01 12:18:56 +0200

    fixed some js and added comments

--- js/script.js
+++ js/script.js
@@ -69,102 +69,117 @@ window.onload = function(){
    //    elem.addEventListener("click", startDownload);
    // })
 
-   var articleParent = document.querySelector("#bekende-personen .horizontal-scroll");
-   var timesClicked = 0;
+   /* 
+         Eerst wilde ik erachter komen hoe ik eventlisteners kon plaatsen op alle knoppen.
+         Met een querySelectorAll ontving ik een nodelist. Een nodelist heeft geen foreach method.
+         Maar je kunt wel Array.from(nodelist).forEach(...) gebruiken. Zo kun je alsnog de method van de array toepassen
+         op de nodelist. Maar dat was niet de ideale manier.
 
-   articleParent.addEventListener('click', (e)=>{
-      // e.stopPropagation();
-      var element = e.target;
-      console.log(element)
-      if(element.tagName.toLowerCase() == "button"){
-         // element.addEventListener('click', startDownload);
-         startDownload(element);
-         // element.click();
-         console.log('fef');
-      }
-   })
-   var downloads = 0;
-   var downloaded = [];
+         Joost kwam met de tip event delegation te gebruiken. Dus daarin heb ik mij verdiept.
+         Nu pas ik een eventlistener toe op de parent van alle articles. Als er ergens binnen die parent wordt geklikt,
+         kan ik de target achterhalen. De tagname van de het elemtent maak ik in een if statement lowercase en dan vergelijk ik
+         die met "button". Op basis daarvan call ik de function startDownload met het element als argument.
+         
+         downloaded is een array lege array die ik opvul met de id's van de verhalen op het moment dat deze worden gedownload.
+         Op het moment dat iemand nogmaals klikt op hetzelfde element, zoek ik de id in de array en slice ik die eruit.
+         
+         downloads daarintegen fungeert alleen om het totaal aantal downloads te tellen zodat ik deze kan weergeven bij de
+         link naar de download pagina.
+   */
 
-   function startDownload(target){
-      console.log(`${target.id} ${this}  test`);
-      var counter = 0;
-      
-      var note = document.querySelectorAll('li a span[data-display="counter"]');
+   if(document.querySelector("#bekende-personen .horizontal-scroll")){
+      var articleParent = document.querySelector("#bekende-personen .horizontal-scroll");
+      var timesClicked = 0;
 
-      var button = this;
+      articleParent.addEventListener('click', (e)=>{
+         // e.stopPropagation();
+         var element = e.target;
+         console.log(element)
+         if(element.tagName.toLowerCase() == "button"){
+            // element.addEventListener('click', startDownload);
+            startDownload(element);
+            // element.click();
+            console.log('fef');
+         }
+      })
+      var downloads = 0;
+      var downloaded = [];
 
-      // timesClicked++;
-      // console.log(timesClicked);
+      function startDownload(target){
+         console.log(`${target.id} ${this}  test`);
+         var counter = 0;
+         
+         // var note = document.querySelectorAll('li a span[data-display="counter"]');
 
-      
+         // var button = this;
 
-      function circleUp(target){
+         // timesClicked++;
+         // console.log(timesClicked);
 
-         target.classList.add("intermediate");
-         // console.log("haahaha")
- 
-         var test = setInterval(function(){
-            counter++;
-            // console.log('fwefwef')
-            console.log(counter)
+         function circleUp(target){
 
-            if (counter == 100){
-               clearInterval(test);
-               console.log("hahahahaha")
-               target.classList.remove("intermediate");
-               target.classList.add("active");
-               
-               // counterDisplay.textContent = "Verwijder";
-               
-            }
-         }, 20);
+            target.classList.add("intermediate");
+            // console.log("haahaha")
+   
+            var timer = setInterval(function(){
+               counter++;
+               // console.log('fwefwef')
+               console.log(counter)
+
+               if (counter == 100){
+                  clearInterval(timer);
+                  console.log("hahahahaha")
+                  target.classList.remove("intermediate");
+                  target.classList.add("active");
+                  // counterDisplay.textContent = "Verwijder";
+               }
+            }, 20);
+
+            // if(timesClicked % 2 == 0){
+            //    target.classList.remove("intermediate");
+            //    target.classList.remove("active");
+            //    clearInterval(test);
+            // }
+
+         }
+
+         if(downloaded.includes(target.id)){
+            target.classList.remove("active");
+            target.classList.remove("intermediate");
+            // console.log(target.id)
+
+            var index = downloaded.indexOf(target.id);
+            downloaded.splice(index, 1);
+         
+            console.log("if "+ downloaded);
+            // console.log(downloaded)
+            // downloaded.pop(e.target.id);        
+         }
+         else{
+            // target.classList.add("intermediate");
+            circleUp(target);
+            downloaded.push(target.id);
+            downloads++;
+            console.log('else '+ downloaded);
+         }
 
          // if(timesClicked % 2 == 0){
-         //    target.classList.remove("intermediate");
-         //    target.classList.remove("active");
-         //    clearInterval(test);
+         //    console.log("off ");
+         //    counter = 0;
+         //    button.classList.remove("active");
+         //    button.classList.remove("intermediate");
          // }
 
-      }
-
-      if(downloaded.includes(target.id)){
-         target.classList.remove("active");
-         target.classList.remove("intermediate");
-         // console.log(target.id)
-         var index = downloaded.indexOf(target.id);
-         
-         downloaded.splice(index, 1);
-           
-       
-         console.log("if "+ downloaded);
-         // console.log(downloaded)
-         // downloaded.pop(e.target.id);
+         // else{
+         //    downloads++;
+         //    note.forEach((elem)=>{
+         //       elem.classList.add("counter");
+         //       elem.textContent = String(downloads);
+         //    })
             
+         //    console.log("on");
+         //    circleUp();
+         // }
       }
-      else{
-         // target.classList.add("intermediate");
-         circleUp(target);
-         downloaded.push(target.id);
-         console.log('else '+ downloaded);
-      }
-
-      // if(timesClicked % 2 == 0){
-      //    console.log("off ");
-      //    counter = 0;
-      //    button.classList.remove("active");
-      //    button.classList.remove("intermediate");
-      // }
-
-      // else{
-      //    downloads++;
-      //    note.forEach((elem)=>{
-      //       elem.classList.add("counter");
-      //       elem.textContent = String(downloads);
-      //    })
-         
-      //    console.log("on");
-      //    circleUp();
-      // }
-   }
-};
+   };
+}
